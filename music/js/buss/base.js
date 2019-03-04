7a6349c2d346a4e1cfde4c53d89eedbdc1dc2399
OneTreeG <alo@163.com>
2019-03-04 14:06:25 +0800

    原始代码V1.0

---
+++ music/js/buss/base.js
@@ -0,0 +1,12 @@
+//1. ��meta��ǩ
+//2. ��ֹȫ�ֵ�Ĭ����Ϊ
+var wrapNode = document.querySelector("#wrap");
+wrapNode.addEventListener("touchstart",function (ev) {
+    ev = ev||event;
+    ev.preventDefault();
+})
+//3.rem����
+var styleNode = document.createElement("style");
+var w = document.documentElement.clientWidth/16;
+styleNode.innerHTML="html{font-size:"+w+"px!important}"
+document.head.appendChild(styleNode);
